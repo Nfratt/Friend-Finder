8b0323488a41c3ebee24ac84cce19dffea6e28c2
Nick Frattaroli <Nafratt@gmail.com>
2019-11-06 13:01:57 -0500

    adding questions

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -14,18 +14,18 @@ app.use(express.json());
 var friendlist = [
   {
     name: 'Nick',
-    photo: '',
+    photo: 'https://media.licdn.com/dms/image/C4D03AQEU1Qvwj1Nhmg/profile-displayphoto-shrink_200_200/0?e=1578528000&v=beta&t=NlH_IiDcJ4ggBZixfDco56tfMLb3c-DT4UuE-Mpk6OU',
     scores: [
+      '5',
+      '2',
+      '5',
+      '5',
+      '4',
       '1',
       '4',
-      '5',
-      '3',
+      '4',
       '1',
-      '1',
-      '1',
-      '2',
-      '1',
-      '3'
+      '1'
     ]
   }
 ]
